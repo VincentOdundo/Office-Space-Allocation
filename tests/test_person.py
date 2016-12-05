63fed0ab54bb20c2519e131f47ae9853ce2ae580
neddoz <kdenno25@gmail.com>
2016-12-05 23:42:48 +0300

    A base module containig the tests to the class person and its subsidiary classes that expose the type of person

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,30 +1,37 @@
 import unittest
-from models.person import Person, Fellow
+from models.person import Person, Fellow, Staff
 """
 Base class for all the tests regarding Person, Fellow and Staff Classes
 """
 
 class TestClassPerson(unittest.TestCase):
 
-    #create Objects from the classes person, Fellow, and Staff
+    """ create Objects from the classes person, Fellow, and Staff """
     def setUp(self):
-        self.person = Person('kayeli', 'dennis', 'Y')
-        self.fellow = Fellow()
+        #instantiate objects from the model classes regarding a person entity
+        self.person = Person('kayeli', 'dennis')
+        self.fellow = Fellow('kayeli', 'dennis')
+        self.staff = Staff('kayeli', 'dennis')
 
-    # test to check if an object can be created from the class Person
+    """ test to check both class Fellow and Staff are subclass to class Person"""
+    def test_fellow_staff_subclass(self):
+        self.assertTrue(issubclass(Fellow, Person))
+        self.assertTrue(issubclass(Staff,Person))
+
+
+    """ test to check if an object can be created from the class Person """
     def testIsInstance(self):
         self.assertIsInstance(self.person, Person)
-
-    #test to check an instance of the class person has all the attributes
-    def test_person_attributes(self):
-        self.assertTrue(hasattr(self.person, 'type'))
-        self.assertTrue(hasattr(self.person, 'first_name'))
-        self.assertTrue(hasattr(self.person, 'second_name'))
-
-    #test to check an instance can be created from the class Fellow
-    def testIsInstance(self):
         self.assertIsInstance(self.fellow, Fellow)
 
-    #test for atttributes of a Fellow
+    """ test to check an instance of the class person has all the attributes """
+    def test_person_attributes(self):
+        self.assertEqual(self.staff.person_type, 'Staff')
+        self.assertTrue(hasattr(self.staff, 'first_name'))
+        self.assertTrue(hasattr(self.staff, 'second_name'))
+
+    """ test for atttributes of a Fellow """
     def test_fellow_attributes(self):
-        self.assertTrue()
+        self.assertEqual(self.fellow.person_type, 'Fellow')
+        self.assertTrue(hasattr(self.fellow, 'first_name'))
+        self.assertTrue(hasattr(self.fellow, 'second_name'))

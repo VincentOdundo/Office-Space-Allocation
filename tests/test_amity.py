36df8ca266f59cf7d40f0047b77616e6b430029f
neddoz <kdenno25@gmail.com>
2016-12-05 23:41:34 +0300

    a finiship up of the assertions regarding the properties of the class Anity

--- tests/test_amity.py
+++ tests/test_amity.py
@@ -2,25 +2,29 @@ import unittest
 
 from models.amity import Amity
 
+"""
+Base class for all the tests regarding Amity, and the Class Room
+"""
+
 class TestClassAmity(unittest.TestCase):
     def setUp(self):
         self.Facility = Amity()
 
-    # test to check if an object can be created from the class Amity
+    """ test to check if an object can be created from the class Amity """
     def testIsInstance(self):
         self.assertIsInstance(self.Facility, Amity)
 
-    #test object has Amity class variables
+    """ test object has Amity class variables """
     def test_amity_class_variables(self):
         self.assertEqual(self.Facility.name, 'Amity')
 
-    #test whether the name attribute of class Amity can be modified
+    """ test whether the name attribute of class Amity can be modified """
     def test_amity_class_attributes_modification(self):
         setattr(self.Facility, self.Facility.name, 'Valhalla')
         self.assertNotEqual(self.Facility.name, 'Valhalla')
 
+    """ test to attest class Amity has all the methods """
     def test_amity_properties(self):
-        #test to see amity has all the methods
         self.assertTrue(hasattr(self.Facility, 'create_room'))
         self.assertTrue(hasattr(self.Facility, 'add_person'))
         self.assertTrue(hasattr(self.Facility, 'reallocate_person'))

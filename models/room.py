52bc98d44ebc9a72171e316243d7573082ca6040
neddoz <kdenno25@gmail.com>
2016-12-05 23:44:14 +0300

    Class room that provides for instantiatiing an object room that could either be a livivng space or office

---
+++ models/room.py
@@ -0,0 +1,26 @@
+"""
+Base class that the class LivingSpace and Office inherit from
+"""
+
+class Room(object):
+    """ constructor to the room class to instantiate a room with a name"""
+    def __init__(self, room_name):
+        self.room_name = room_name
+
+"""
+Class LivingSpace is to expose a room of type LivingSpace and the number of occupants
+"""
+class LivingSpace(Room):
+    def __init__(self, room_name):
+        super().__init__(room_name)
+        self.room_name = room_name
+        self.no_of_occupants = 6
+
+"""
+Class LivingSpace is to expose a room of type Office and the number of occupants
+"""
+class Office(Room):
+    def __init__(self, room_name):
+        super().__init__(room_name)
+        self.room_name = room_name
+        self.no_of_occupants = 4

7d190e2828fabdbf676a97260e062db457acb11f
neddoz <kdenno25@gmail.com>
2016-12-08 11:17:21 +0300

    refactoring code for tests regarding a person class

--- tests/test_person.py
+++ tests/test_person.py
@@ -17,21 +17,3 @@ class TestClassPerson(unittest.TestCase):
     def test_fellow_staff_subclass(self):
         self.assertTrue(issubclass(Fellow, Person))
         self.assertTrue(issubclass(Staff,Person))
-
-
-    """ test to check if an object can be created from the class Person """
-    def testIsInstance(self):
-        self.assertIsInstance(self.person, Person)
-        self.assertIsInstance(self.fellow, Fellow)
-
-    """ test to check an instance of the class person has all the attributes """
-    def test_person_attributes(self):
-        self.assertEqual(self.staff.person_type, 'Staff')
-        self.assertTrue(hasattr(self.staff, 'first_name'))
-        self.assertTrue(hasattr(self.staff, 'second_name'))
-
-    """ test for atttributes of a Fellow """
-    def test_fellow_attributes(self):
-        self.assertEqual(self.fellow.person_type, 'Fellow')
-        self.assertTrue(hasattr(self.fellow, 'first_name'))
-        self.assertTrue(hasattr(self.fellow, 'second_name'))
